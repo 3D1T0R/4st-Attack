655ae1b33ef37f4259d6b2630b284f54f31e4f19
3D1T0R <3D1T0R@users.noreply.github.com>
2018-11-04 06:53:50 -0700

    Set exe version to 2.1.3

--- pygame2exe.py
+++ pygame2exe.py
@@ -54,7 +54,7 @@ class BuildExe:
         self.project_url = "http://forcedattack.sf.net/"
 
         #Version of program
-        self.project_version = "2.1.2"
+        self.project_version = "2.1.3"
 
         #License of the program
         self.license = "GPL"
